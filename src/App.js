24bb333632b8267869da2f08e3299286e6c88966
Luke Lorenger <dirtydeadtv@gmail.com>
2021-05-22 08:10:19 -0400

    attached css properties

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import Header from './components/Header'
+import React from 'react'
+
 function App() {
+
   return (
-    <div className="App">
-      <h1>Hello Handsome</h1>
+    <div className='container'>
+      <Header />
     </div>
   );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 
 export default App;
