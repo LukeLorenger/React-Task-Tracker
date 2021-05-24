d81cdf05bd0bbb2c4a8de99f7d89d8eee3c92cd5
Luke Lorenger <dirtydeadtv@gmail.com>
2021-05-24 18:36:05 -0400

    Add Task, Delete Task, Set Reminder all function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import {useState} from 'react'
 
 const App = () => {
@@ -24,15 +25,30 @@ const App = () => {
     }
 ])
 
+// Add Task
+const addTask = (task) => {
+  // console.log(task)
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
+
 // Delete Task
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id
+  ? { ...task, reminder: !task.reminder } : task))
+}
+
   return (
     <div className='container'>
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Task To Show'}
     </div>
   );
 }
